728c0039cfb1394bdbbcea8a6cadf1821a5ca04c
BelowZeroKelvin <63833182+BelowZeroKelvin@users.noreply.github.com>
2021-01-16 20:48:27 +0800

    Adapt to MCDR1.0

--- Daytime.py
+++ Daytime.py
@@ -1,3 +1,17 @@
+#coding: utf-8
+
+PLUGIN_METADATA = {
+	'id': 'daytime',
+	'version': '1.0.0',
+	'name': 'Daytime',  # RText component is allowed
+	'description': 'show time in minecraft',  # RText component is allowed
+	'author': 'ZeroKelvin',
+	'link': 'https://github.com/BelowZeroKelvin/MCDR-Daytime/blob/master/Daytime.py',
+	'dependencies': {
+		'mcdreforged': '>=0.9.0',
+	}
+}
+
 daytime = 0
 daytime_updated = True
 
@@ -11,13 +25,11 @@ def daytime_format(daytime):
 
 def on_info(server, info):
     global daytime_updated, daytime
-    if info.source == 0 and not info.is_player and info.content.startswith(
+    if info.source == 0 and daytime_updated==False and not info.is_player and info.content.startswith(
             'The time is'):
         daytime = info.content[12:]
+        server.say(daytime_format(daytime))
         daytime_updated = True
     elif info.is_player and info.content == '!!daytime':
         daytime_updated = False
         server.execute('time query daytime')
-        while not daytime_updated:
-            pass
-        server.say(daytime_format(daytime))
